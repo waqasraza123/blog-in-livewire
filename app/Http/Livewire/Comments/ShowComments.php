e4ef0e364f5c1debb100dc1b27f19c726841ae07
Waqas Raza <waqasraza123@gmail.com>
2022-12-13 03:10:48 +0500

    minor tweaks

--- app/Http/Livewire/Comments/ShowComments.php
+++ app/Http/Livewire/Comments/ShowComments.php
@@ -12,19 +12,33 @@ class ShowComments extends Component
     protected $paginationTheme = 'bootstrap';
     public $comments;
 
+    /*
+     * event listeners
+     */
     public function getListeners()
     {
+        //return ["eventName" => "listenerName"];
+        //return ["commentAdded" => "commentAdded"];
         return ['commentAdded'];
     }
 
+    /*
+     * runs on mount once
+     */
     public function mount(){
         $this->comments = Comment::latest()->get();
     }
 
+    /*
+     * load comments when a new comment is added
+     */
     public function commentAdded(){
         $this->comments = Comment::latest()->get();
     }
 
+    /*
+     * render
+     */
     public function render()
     {
         return view('livewire.comments.show-comments');
