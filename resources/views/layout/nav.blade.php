3a406ecb809e8ea37762a0278b7ffeb3022fc266
Waqas Raza <waqasraza123@gmail.com>
2022-12-15 22:23:22 +0500

    added authentication via jetstream

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar bg-light">
     <div class="container-fluid">
         <a class="navbar-brand">Crypto Blog</a>
-        <a href="{{url("/")}}" class="navbar-brand">Home</a>
+        <a href="{{route("dashboard")}}" class="navbar-brand">Dashboard</a>
         <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Posts\SinglePost;
 
-Route::get('/', function () {
-    return view('app');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/', function () {
+        return view('app');
+    })->name('home');
+
+    Route::get('/posts/{post}', SinglePost::class)
+        ->name('posts.single');
 });
-Route::get('/posts/{post}', SinglePost::class)->name('posts.single');
